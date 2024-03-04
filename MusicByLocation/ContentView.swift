f26d279d436f4ae5f83be2e630b9011dedf369bf
Josh Kim <kim.j@etoncollege.org.uk>
2024-03-04 20:00:46 +0000

    Initial Commit

---
+++ MusicByLocation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicByLocation
+//
+//  Created by Kim, Joshua (WING) on 04/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Kim, Joshua (WING) on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
